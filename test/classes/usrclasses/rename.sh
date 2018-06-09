257b2c56980334edcb52d43ec89388327fe74ca3
HBaena <39740586+HBaena@users.noreply.github.com>
2018-06-09 02:02:17 -0500

    Delete rename.sh

--- test/classes/usrclasses/rename.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#ls lista el contenido de la carpeta
-#lista sólo los .jpg | sed busca los jpg
-FILES=$(ls *.jad | sed 's/.jad//' )
-for i in $FILES; do
-    echo $i'.jpg -->' $i'.png'
-    cp $i'.jad' 2> /dev/null | pnmtopng > $i'.java' 
-done
